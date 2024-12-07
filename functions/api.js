147e4d46f616b10fd202556d32d6c64673a04bd0
dragondevsimplify <dragondevsimplify@gmail.com>
2024-12-07 10:30:33 +0700

    Setup project

---
+++ functions/api.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import ServerlessHttp from "serverless-http";
+const axios = require('axios');
+import { XMLParser } from "fast-xml-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const parser = new XMLParser();
+
+app.get('/.netlify/functions/api', (req, res) => {
+  return res.json({
+    message: "Hello"
+  })
+})
+
+app.post('/.netlify/functions/api', async (req, res) => {
+  try {
+    const { body } = req
+    
+    const parcelUrl = body['parcelUrl']
+    const sessionId = body['sessionId']
+    const parcelIds = body['parcelIds']
+    
+    if (!body || !parcelUrl || !sessionId || !parcelIds) {
+      return res.status(400).send('Required: parcelUrl, sessionId, parcelIds')
+    }
+    
+    const apisMap = []
+    
+    if (Array.isArray(parcelIds)) {
+      parcelIds.forEach((parcelId) => {
+        const url = `${parcelUrl}`
+        apisMap.push([url, {
+          'SessionID': sessionId,
+          'ParcelID': parcelId,
+        }])
+      })
+    }
+    
+    const response = await Promise.all(apisMap.map(async ([url, params]) => {
+      const xmlRes = await axios.get(url, { params })
+      const dataParsed = parser.parse(xmlRes.data)
+      
+      const fileInfo = dataParsed['FileInfo']
+      const contentBase64 = fileInfo['ContentBase64String']
+      
+      let contentDecoded = 'Not found content to parse !!!'
+      
+      if (fileInfo && contentBase64) {
+        const buffer = Buffer.from(contentBase64, 'base64');
+        contentDecoded = buffer.toString('utf8');
+      }
+      
+      return {
+        ...dataParsed,
+        contentDecoded,
+      }
+    }));
+    
+    res.json(response);
+  } catch (error) {
+    console.error('Error calling the API:', error);
+    res.status(500).send('Error fetching data from Parcel URL');
+  }
+});
+
+
+const handler = ServerlessHttp(app);
+
+module.exports.handler = async (event, context) => {
+  return await handler(event, context);
+}
---
+++ src/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const parser = new XMLParser();
+
+app.post('/', async (req, res) => {
+  try {
+    const { body } = req
+    
+    const parcelUrl = body['parcelUrl']
+    const sessionId = body['sessionId']
+    const parcelIds = body['parcelIds']
+    
+    if (!body || !parcelUrl || !sessionId || !parcelIds) {
+      return res.status(400).send('Required: parcelUrl, sessionId, parcelIds')
+    }
+    
+    const apisMap = []
+    
+    if (Array.isArray(parcelIds)) {
+      parcelIds.forEach((parcelId) => {
+        const url = `${parcelUrl}`
+        apisMap.push([url, {
+          'SessionID': sessionId,
+          'ParcelID': parcelId,
+        }])
+      })
+    }
+    
+    const response = await Promise.all(apisMap.map(async ([url, params]) => {
+      const xmlRes = await axios.get(url, { params })
+      const dataParsed = parser.parse(xmlRes.data)
+      
+      const fileInfo = dataParsed['FileInfo']
+      const contentBase64 = fileInfo['ContentBase64String']
+      
+      let contentDecoded = 'Not found content to parse !!!'
+      
+      if (fileInfo && contentBase64) {
+        const buffer = Buffer.from(contentBase64, 'base64');
+        contentDecoded = buffer.toString('utf8');
+      }
+      
+      return {
+        ...dataParsed,
+        contentDecoded,
+      }
+    }));
+    
+    res.json(response);
+  } catch (error) {
+    console.error('Error calling the API:', error);
+    res.status(500).send('Error fetching data from API');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
